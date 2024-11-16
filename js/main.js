fb5170463943c82c02cc48793a4169de9ab0bd75
CarlJohn120 <156630663+CarlJohn120@users.noreply.github.com>
2024-11-16 16:15:45 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//Import the THREE.js library
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 // To allow for the camera to move around the scene
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
@@ -10,7 +9,7 @@ const scene = new THREE.Scene();
 //create a new camera with positions and angles
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-//Keep track of the mouse position, so we can make the eye move
+//Keep track of the mouse position, so we can make the dino skull move
 let mouseX = window.innerWidth / 2;
 let mouseY = window.innerHeight / 2;
 
@@ -71,11 +70,9 @@ if (objToRender === "dino") {
 //Render the scene
 function animate() {
   requestAnimationFrame(animate);
-  //Here we could add some code to update the scene, adding some automatic movement
 
-  //Make the eye move
+  //Make the dino skull move
   if (object && objToRender === "dino") {
-    //I've played with the constants here until it looked good 
     object.rotation.y = -3 + mouseX / window.innerWidth * 3;
     object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
   }
@@ -89,11 +86,10 @@ window.addEventListener("resize", function () {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-//add mouse position listener, so we can make the eye move
+//add mouse position listener, so we can make the dino skull move
 document.onmousemove = (e) => {
   mouseX = e.clientX;
   mouseY = e.clientY;
 }
 
-//Start the 3D rendering
 animate();
